3a95915326cd292389f19ccec121174ea77874cf
multi <multi@AFEQUIPO15>
2020-09-23 17:43:16 +0200

    Creado archivo tipos de datos

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,11 +3,14 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
+		byte edad=4;
+		
 		//Prueba de un Hola Mundo 1º DAM
 		System.out.println("Hola Mundo");
 		System.out.print("Buenas tardes");
 		System.out.println();
-		System.out.print("Prueba con ñ y tildes á é í ó ú");
+		System.out.println("Prueba con ñ y tildes á é í ó ú");
+		System.out.println("La edad es:" + edad);
 		
 		
 		/*Prueba de un comentario
