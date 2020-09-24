150f7f40fb389addbec8d9aecf6c8860772c6dbc
cjodra14 <c.jodra14@gmail.com>
2020-09-24 13:48:29 +0200

    Ejercicios 3 y 4

--- src/Ejer1.java
+++ src/Ejer1.java
@@ -1,13 +1,42 @@
-//Programa que visualiza datos del centro de Estudios: Ceinmark.
+
 public class Ejer1 {
 
+	//Ejercicios 1 y 2
+	//Programa que visualiza datos del centro de Estudios: Ceinmark.	
 	public static void main(String[] args) {
 		System.out.println("Ciclo: Desarrollo de Aplicaciones Web");
-		System.out.println("Módulo: Programación");
+		System.out.println("Mï¿½dulo: Programaciï¿½n");
 		System.out.println("Centro: Ceinmark");
 		System.out.println("Calle: Iturribide 22-24-26");
 		System.out.println("Telef: 944157298");
 		System.out.println( "Web: www.ceinmark.com");
+		System.out.println();
+	
+//Ejercicios 3
+		byte n1=1;
+		short n2=1000;
+		int n3=25000;
+		long n4=-3000000L;
+		float n5=3.14f;
+		double n6=-2.5468412d;
+		boolean n7=true;
+		char n=',';
+		
+//VisualizaciÃ³n de las variables separadas por comas, utilizando un char como coma.
+		
+	System.out.println("Valores de las variables "+n1+n+n2+n+n3+n+n4+n+n5+n+n6+n+n7);
+	System.out.println();
+	
+//VisualizaciÃ³n de los datos en una columna
+	System.out.println(n1);
+	System.out.println(n2);
+	System.out.println(n3);
+	System.out.println(n4);
+	System.out.println(n5);
+	System.out.println(n6);
+	System.out.println(n7);
+	System.out.println(n);
+		
 	}
 
 }
