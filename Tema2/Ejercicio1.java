f98e908cf933a59eb8d145f7d55071b827056e08
multi <multi@A1EQUIPO09>
2020-09-30 19:23:49 +0200

    Ejercicio 1 Tema 2

---
+++ Tema2/Ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int numero;
+		
+	//Entrada del numero
+		System.out.println("Teclea un numero:");
+		numero=teclado.nextInt();
+		if(numero>0) {
+			System.out.println("El numero "+numero+" es positivo");
+		}
+		else if(numero<0) {
+			System.out.println("El numero "+numero+" es negativo");
+		}
+		else {
+			System.out.println("El numero es nulo");
+		}
+		
+	}
+
+}
