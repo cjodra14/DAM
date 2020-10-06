c0bbc430fea0e1ecceb8b743a046d63eae625407
multi <multi@A1EQUIPO09>
2020-10-06 18:33:07 +0200

    IfNatacion e pruebas con if seguidos

--- Tema2/PruebasConIf.java
+++ Tema2/PruebasConIf.java
@@ -10,15 +10,67 @@ public class PruebasConIf {
 		
 //-------------------------------------------------------
 		if() {
-			
-			
-			
-			
+			//primer if si
+			if() {
+				//segundo if no
+				
+				
+			}
+			else {
+				if() {
+					
+					//tercer if no
+				}
+				else {
+					if() {
+						//cuarto if si
+						if() {
+							//quinto if si
+							if() {
+								//sexto if no
+							}
+							else {
+								if() {
+									//Fin
+								}
+								else {
+									
+								}
+								
+							}
+						}
+						else {
+							
+						}
+						
+					}
+					else {
+						
+					}
+				}
+				
+				
+			}
+				
 			
 		}
 		else {
 			
-			
+			//primer else no
+			if() {
+				
+				
+			}
+			else {
+				//segundo else no
+				if() {
+					
+				}
+				else {
+					//Fin
+				}
+				
+			}
 			
 			
 		}
