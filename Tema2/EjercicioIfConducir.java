5517a2efd006ea48ff934f3f2eff87800642e028
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2020-10-07 14:30:17 +0200

    If conducir

---
+++ Tema2/EjercicioIfConducir.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class EjercicioIfConducir {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		byte edad;
+		int cc, mma;
+		char tipoVehic='a';
+		
+		
+		
+//-----------------------------------------
+		System.out.println("¿Para que vehiculo quieres obtener el permiso?(a para coche, m para moto");
+		if(tipoVehic=='m') {
+			
+		}
+		else {
+			System.out.println("¿Cual es la mma del automovil que desea conducir?");
+			mma=teclado.nextInt();
+			if(mma<3500) {
+				System.out.println("Tecelee su edad:");
+				edad=teclado.nextByte(); 
+				if(edad>=18) {
+					System.out.println("PUEDES TENER PERMISO DE CONDUCCIÓN");
+					
+				}
+				else {
+					
+				}
+			}
+			else {
+				
+			}
+			
+		}
+		
+
+	}
+
+}
