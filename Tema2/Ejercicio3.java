d7c430912daef5992064f7fa25bd4aea49529037
multi <multi@A1EQUIPO09>
2020-10-08 20:38:14 +0200

    Creacion del ejercicio 3

---
+++ Tema2/Ejercicio3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner src=new Scanner(System.in);
+	
+		
+	}
+
+}
