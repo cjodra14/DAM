8fafd9d1c704cd8c5e45f3725dc150ad8b008aa4
multi <multi@A1EQUIPO09>
2020-10-28 19:01:10 +0100

    Ejer 15 empezado

---
+++ Tema2/Ejer15.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejer15 {
+
+	public static void main(String[] args) {
+		Scanner src=new Scanner(System.in);
+		int tablasNumero;
+		
+		for(int i=1;i<=10;i++) {
+			
+		}
+		
+	}
+}
