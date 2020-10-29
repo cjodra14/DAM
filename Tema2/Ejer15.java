0c34d32ccb0a558e99453f7752387dc9cfd11635
multi <multi@A1EQUIPO09>
2020-10-29 18:38:15 +0100

    Ejer15 subir de nuevo, cincoNumeros terminado

--- Tema2/Ejer15.java
+++ Tema2/Ejer15.java
@@ -5,8 +5,8 @@ public class Ejer15 {
 		Scanner src=new Scanner(System.in);
 		 final int PRIMERO=1, SEGUNDO=2, TERCERO=3, CUARTO=4, QUINTO=5;
 			
-		for(int i=1;i<=10;i++) {
-			System.out.println(PRIMERO+"x"+i+"="+PRIMERO*i+"	"+SEGUNDO+"x"+i+"="+SEGUNDO*i+"	"+TERCERO+"x"+i+"="+TERCERO*i+"	"+CUARTO+"x"+i+"="+CUARTO*i+"	"+QUINTO+"x"+i+"="+QUINTO*i);
-		}
+			for(int i=1;i<=10;i++) {
+				System.out.println(PRIMERO+"x"+i+"="+PRIMERO*i+"	"+SEGUNDO+"x"+i+"="+SEGUNDO*i+"	"+TERCERO+"x"+i+"="+TERCERO*i+"	"+CUARTO+"x"+i+"="+CUARTO*i+"	"+QUINTO+"x"+i+"="+QUINTO*i);
+			}
 	}
 }
