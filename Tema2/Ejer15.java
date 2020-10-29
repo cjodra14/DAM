be734345c063010c9f8920694e637760bb224d45
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2020-10-29 12:04:47 +0100

    Ejer15 terminado

--- Tema2/Ejer15.java
+++ Tema2/Ejer15.java
@@ -3,11 +3,10 @@ public class Ejer15 {
 
 	public static void main(String[] args) {
 		Scanner src=new Scanner(System.in);
-		int tablasNumero;
-		
-		for(int i=1;i<=10;i++) {
+		 final int PRIMERO=1, SEGUNDO=2, TERCERO=3, CUARTO=4, QUINTO=5;
 			
+		for(int i=1;i<=10;i++) {
+			System.out.println(PRIMERO+"x"+i+"="+PRIMERO*i+"	"+SEGUNDO+"x"+i+"="+SEGUNDO*i+"	"+TERCERO+"x"+i+"="+TERCERO*i+"	"+CUARTO+"x"+i+"="+CUARTO*i+"	"+QUINTO+"x"+i+"="+QUINTO*i);
 		}
-		
 	}
 }
