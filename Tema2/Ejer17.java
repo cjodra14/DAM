401550ac47289f08794684bf39d5aaed72399fb9
multi <multi@A1EQUIPO09>
2020-11-03 18:44:50 +0100

    Inicio ejer20

--- Tema2/Ejer17.java
+++ Tema2/Ejer17.java
@@ -12,22 +12,22 @@ public class Ejer17 {
 		n2=src.nextInt();
 		
 		for(i=1;i<n1;i++) {
-			if(n1%i==0) {
-				suma=suma+i;
+			if(n1%i==0) { // Si el resto es igual a 0 entre en el if
+				suma=suma+i; // a suma le añade el divisor
 			}
 			
 		}
 		
-		if(suma==n2) {
-			suma=0;
-			for(i=1;i<n2;i++) {
-				if(n2%i==0) {
-					suma=suma+i;
+		if(suma==n2) { //Comprueba si  la suma de divisores es igual al segundo numero introducido
+			suma=0; //Iguala suma a 0
+			for(i=1;i<n2;i++) { 
+				if(n2%i==0) { //si el resto de el segundo numero es 0 entra en el if
+					suma=suma+i; // Le añade divisor a suma
 				}
 				
 			}
 			if(suma==n1) {
-				System.out.println("Son amigos");
+				System.out.println("Son amigos"); //Compara si suma es iguala  1
 			}else {
 				System.out.println("No son amigos");
 			}
@@ -36,6 +36,8 @@ public class Ejer17 {
 		else {
 			System.out.println("No son amigos");
 		}
+		
+		src.close();
 			
 		
 
