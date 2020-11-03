afda25ccc40263433e2b8b78b626bf9e951093de
multi <multi@A1EQUIPO09>
2020-11-03 19:37:28 +0100

    Ejer 20 continuacion

--- Tema2/Ejer20.java
+++ Tema2/Ejer20.java
@@ -4,7 +4,8 @@ public class Ejer20 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner src=new Scanner(System.in);
-		 int numero, baseIni, baseFin;
+		 double numero, baseIni, baseFin, suma;
+		 String convertido="";
 		 
 		 System.out.println("Introduza el numero que desea convertir:");
 		 numero=src.nextInt();
@@ -14,10 +15,14 @@ public class Ejer20 {
 		 baseFin=src.nextInt();
 		 if(baseIni>baseFin) {
 			 //Este es el camino que hace si es de una base mayor a una menor P.E. de decimal a binario
-			 
-			 
+			 while(numero>0) {
+				 convertido=(numero%baseFin)+convertido;
+				 numero/=baseFin;
+			 }
+			System.out.println(convertido);
 		 }else {
 			 //Este camino que hace si es de una base menor a otra mayor P.E. de binario a decimal
+			 
 		 }
 		 
 		 
