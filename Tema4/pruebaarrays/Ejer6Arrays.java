8ce3428b8e55792c40b7f26f4cd9cc51450a5962
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2020-12-11 12:59:06 +0100

    Ejercicio 6 hecho bien

--- Tema4/pruebaarrays/Ejer6Arrays.java
+++ Tema4/pruebaarrays/Ejer6Arrays.java
@@ -1,34 +1,114 @@
 package pruebaarrays;
-import java.lang.Math;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejer6Arrays {
-	int n[]=new int[6];
 	
-	void lista() {
-		for(int x=0;x<n.length;x++) {
-			n[x]=(int)((Math.random())*101);
-			while(n[x]<1) {
-				n[x]=(int)((Math.random())*101);
-			}
-			while(n[x]>100) {
-				n[x]=(int)((Math.random())*101);
-			}
-			
-		}
-	}
-	void visualizar() {
-		for(int x=0;x<n.length;x++) {
-			System.out.println(n[x]);
-		}
-		
-	}
+    // crear un array de numeros aleatorios mas pequeños que el 100 y luego en
+    // otro array mostrar los numeros mas pequeños que el 20 que no se hayan
+    // generado en el array
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Ejer6Arrays ob=new Ejer6Arrays();
-		ob.lista();
-		ob.visualizar();
+	  Random rand = new Random();
+	  Scanner src = new Scanner(System.in);
+	  int longitud;
+	 
+	  int[] array;
+	  Ejer6Arrays(){
+		  System.out.println("Teclea la longitud deseada para el array");
+			longitud=src.nextInt();
+			array=new int[longitud];
+	  }
+	  
+	 
+	  int numMax() {
+		  int max;
+		  System.out.println("Teclea el numero maximo:");
+		  max=src.nextInt();
+		  return max;
+	  }
+	  int numMin() {
+		  int min;
+		  System.out.println("Teclea el numero minimo:");
+		  min=src.nextInt();
+		  return min;
+	  }
+	  
+	  int numAleatorio(int minimo, int maximo) {
+	    	int num=(int)Math.floor(Math.random()*(minimo-maximo+1)+(maximo));
+	    	return num;
+	    } 
+	  
+	  boolean comprobacion(int max, int min) {
+		  boolean comprobado;
+		  if(max<min) {
+			  int aux=max;
+			  max=min;
+			  min =aux;
+		  }
+		  if((max-min)>=(longitud-1)) {
+			  comprobado=true;
+			  
+		  }else {
+			  System.out.println("No se puede generar, la longitud es mayor a los numeros disponibles");
+			  comprobado=false;
+		  }
+		  return comprobado;
+		  
+	  }
+	  
+	  void generar(boolean comprobado,int max, int min) {
+		  if(comprobado==true) {
+			  int numero_elementos=0;
+			  boolean encontrado;
+			  int aleatorio;
+			  
+			  
+			  while(numero_elementos<longitud) {
+				  
+				  
+				  aleatorio=numAleatorio(max,min);
+				  encontrado=false;
+				  
+				  for(int i=0;i<array.length && !encontrado;i++) {
+					  if(aleatorio==array[i]) {
+						  encontrado=true;
+					  }
+				  }
+				  if(encontrado) {
+					  System.out.println("El numero "+aleatorio+" ya existe ");
+				  }else {
+					  array[numero_elementos]=aleatorio;
+					  numero_elementos++;
+					  System.out.println("Agregado numero: "+aleatorio);
+				  }
+			  }
+			  
+		  }
+	  }
+	  void visu() {
+		  System.out.println();
+		  System.out.println("El array completo es:");
+		  for(int i=0;i<array.length;i++) {
+			  System.out.println(array[i]);
+		  }
+		  
+	  }
+	  
 
-	}
+	    public static void main(String[] args) {
+	    	int max, min;
+	    	boolean comprobado;
+	    	Ejer6Arrays ob=new Ejer6Arrays();
+	    	max=ob.numMax();
+	    	min=ob.numMin();
+	    	comprobado=ob.comprobacion(max, min);
+	    	ob.generar(comprobado, max, min);
+	    	ob.visu();
+	
+	    }
+
+	  
+ 
+    
 
 }
